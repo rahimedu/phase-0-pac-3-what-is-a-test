ba905008dedde5dc5cda878dc6b1bb0bab259f12
rahimedu <edwin.duche82@gmail.com>
2022-06-06 22:49:30 -0400

    message

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+console.log(message);
+describe("Name", () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual("Susan");
+    });
+  });
+  describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(message).toInclude(name);
+      expect(message).toInclude(height);
+    });
+  });
+  describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(message).toInclude(name);
+      expect(message).toInclude(height);
+    });
+  });
